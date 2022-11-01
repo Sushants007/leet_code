e4d505e962f602240108025132642e035a7458cf
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-01 23:05:47 +0530

    Time: 126 ms (54.09%), Space: 18.4 MB (16.11%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,50 +1,31 @@
 class MinStack:
 
     def __init__(self):
-        self.stack=[]
-        self.minele=sys.maxsize
-
-    def push(self, x: int) -> None:
-        if len(self.stack)==0:
-            self.stack.append(x)
-            self.minele=x
-        else:
-            if x>=self.minele:
-                self.stack.append(x)
-            elif x<self.minele:
-                self.stack.append(2*x-self.minele)
-                self.minele=x
-
-    def pop(self) -> None:
-        if self.stack==0:
-            return -1
-        else:
-            if self.stack[-1]>=self.minele:
-                self.stack.pop()
-            elif self.stack[-1]<self.minele:
-                self.minele=2*self.minele-self.stack[-1]
-                self.stack.pop()
-
-    def top(self) -> int:
-        if len(self.stack)==0:
-            return -1
-        else:
-            if self.stack[-1]>=self.minele:
-                return self.stack[-1]
-            elif self.stack[-1]<self.minele:
-                return self.minele
-        
-        
-    def getMin(self) -> int:
-        if self.stack==0:
-            return -1
-        return self.minele
+        self.q=[]
         
 
+    def push(self, x):
+        curMin = self.getMin()
+        if curMin == None or x < curMin:
+            curMin = x
+        self.q.append((x, curMin));
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+    # @return nothing
+    def pop(self):
+        self.q.pop()
+
+
+    # @return an integer
+    def top(self):
+        if len(self.q) == 0:
+            return None
+        else:
+            return self.q[len(self.q) - 1][0]
+
+
+    # @return an integer
+    def getMin(self):
+        if len(self.q) == 0:
+            return None
+        else:
+            return self.q[len(self.q) - 1][1]
