b5fd3dd0340e90d1aa60e72e4f471d5ebdea794b
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-01 16:33:29 +0530

    Time: 334 ms (27.77%), Space: 16.1 MB (20.06%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -6,7 +6,7 @@ class Solution:
         sum=0
         mxl[0]=height[0]
         mxr[len(height)-1]=height[len(height)-1]
-        for i in range(0,len(height),1):
+        for i in range(1,len(height),1):
             mxl[i]=max(mxl[i-1],height[i])
         for i in range(len(height)-2,0,-1):
             mxr[i]=max(mxr[i+1],height[i])
