1c8be52949a17ec5aa9dfd31aea906779113e6ba
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-13 17:36:08 +0530

    Time: 873 ms (70.73%), Space: 14 MB (14.84%) - LeetHub

---
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        if highLimit<10 and highLimit!=lowLimit:
+            return 1
+        cnt, mx = [0] * 46, 0
+        for i in range(lowLimit, highLimit + 1):
+            sm = 0
+            while i > 0:
+                sm += i % 10
+                i //= 10
+            cnt[sm] += 1
+            mx = max(mx, cnt[sm])
+        return mx
