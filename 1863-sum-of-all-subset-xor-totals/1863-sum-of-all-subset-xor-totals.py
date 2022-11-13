9b068221023ce667b8d10fe90e17ce410f7f156c
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-13 15:31:13 +0530

    Time: 97 ms (68.41%), Space: 13.9 MB (71.82%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,14 +1,13 @@
 
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        result = 0
-        subsets = [0]
+        ans=0
+        arr=[0]
         for n in nums:
-            new_subsets = subsets.copy()
-            for s in subsets:
-                new_subsets.append(s ^ n)
-                result += new_subsets[-1]
-                
-            subsets = new_subsets
-                
-        return result        
+            new=arr.copy()
+            for s in arr:
+                new.append(s^n)
+                ans+=new[-1]
+            arr=new
+        return ans
+            
