cfad261734e0dfba1f757e08966e010608550578
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-13 17:24:57 +0530

    Time: 212 ms (60.58%), Space: 13.8 MB (64.98%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
@@ -4,8 +4,8 @@ class Solution:
         while num1>=1 and num2>=1:   
             if num1>num2:
                 num1=num1-num2
-                ct+=1
+                
             else:
                 num2=num2-num1
-                ct+=1
+            ct+=1
         return ct
