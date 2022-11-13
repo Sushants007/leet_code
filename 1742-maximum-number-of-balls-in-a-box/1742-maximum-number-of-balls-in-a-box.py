579ca658155fcae2c24b66a4ab6bdc567bbb383b
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-13 17:44:01 +0530

    Time: 2091 ms (10.54%), Space: 13.9 MB (71.84%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
@@ -1,13 +1,8 @@
 class Solution:
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
-        #if highLimit<10 and highLimit!=lowLimit:
-        #    return 1
-        cnt, mx = [0] * 46, 0
-        for i in range(lowLimit, highLimit + 1):
-            sm = 0
-            while i > 0:
-                sm += i % 10
-                i //= 10
-            cnt[sm] += 1
-            mx = max(mx, cnt[sm])
-        return mx
+        if highLimit<10 and highLimit!=lowLimit:
+            return 1
+        freq = defaultdict(int)
+        for x in range(lowLimit, highLimit+1):
+            freq[sum(int(xx) for xx in str(x))] += 1
+        return max(freq.values())
