62705e9cecd907b361dc0aa4cbb062c2cb0faf71
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-15 16:47:26 +0530

    Time: 109 ms (76.54%), Space: 14.1 MB (31.91%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,18 +1,9 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-        res=[]
-        for num in arr:
-            ones=self.count(num)
-            res.append((ones,num))
-        heapq.heapify(res)
-        finalres=[]
-        while len(res)>0:
-            finalres.append(heappop(res)[1])
-        return finalres
-    def count(self,n):
-        ct=0
-        while n:
-            n&=n-1
-            ct+=1
-            n//=2
-        return ct
+        return sorted(arr, key = lambda x: (self.countOnes(x), x))        
+    def countOnes(self, x: int) -> int:
+        count = 0
+        while x:
+            x &= (x - 1)
+            count += 1
+        return count
