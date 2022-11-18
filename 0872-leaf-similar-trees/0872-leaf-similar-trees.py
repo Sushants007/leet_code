3d6df4047a269e7db89ccfc91dee1abb4b8d97fa
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-18 16:54:34 +0530

    Time: 70 ms (20.49%), Space: 14.1 MB (8.95%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -1,21 +1,24 @@
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        leaf_seq_1 = []
-        leaf_seq_2 = []
-
-        
-        def helper( node: TreeNode, container):
-            
-            if node:
-                
-                helper( node.left, container )
-                
-                if not node.left and not node.right:
-                    container.append( node.val )
-                    
-                helper( node.right, container )
-                
-        helper( root1, leaf_seq_1)
-        helper( root2, leaf_seq_2)
-        
-        return leaf_seq_1 == leaf_seq_2
+        arr1 = []
+        arr2 = []
+        def rec1(root1):
+            if root1 == None:
+                return None
+            if root1.left == None and root1.right == None:
+                arr1.append(root1.val)
+            rec1(root1.left)
+            rec1(root1.right)
+        def rec2(root2):
+            if root2 == None:
+                return None
+            if root2.left == None and root2.right == None:
+                arr2.append(root2.val)
+            rec2(root2.left)
+            rec2(root2.right)
+        rec1(root1)
+        rec2(root2)
+        if arr1 == arr2:
+            return True
+        else:
+            return False
