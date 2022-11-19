96671665addbf0ce5243291591bae342ab7c535b
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-19 18:28:49 +0530

    Time: 46 ms (78.59%), Space: 13.9 MB (91.58%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
@@ -1,13 +1,17 @@
-
-class Solution:
-    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        dict={}
-        tree=[]
-        tree.append((root,0,0))
-        while tree:
-            node,parent,depth=tree.pop()
-            if node:
-                dict[node.val]=(parent,depth)
-                tree.append((node.left,node.val,depth+1))
-                tree.append((node.right,node.val,depth+1))
-        return dict[x][1]==dict[y][1] and dict[x][0]!=dict[y][0]
+class Solution: 
+    def isCousins(self, root, x, y):
+        def dept(node, d, par):
+            if not node: return 
+			
+            if node.val == x:
+                self.a = d
+                self.aparent= par
+	
+            elif node.val == y:
+                self.b = d
+                self.bparent= par
+            dept(node.left, d+1, node)
+            dept(node.right, d+1, node)
+        dept(root, 0, None)
+        # return True if a and b are equal and their parents are not same
+        return self.a == self.b and self.aparent != self.bparent
