c415778c9840ea405e311b2e5a673425619cd6fd
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-19 12:58:42 +0530

    Time: 38 ms (86.57%), Space: 14 MB (30.87%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -12,8 +12,9 @@ class Solution:
             node,ans=q.pop()
             if not node.left and not node.right:
                 res.append(ans+str(node.val))
-            if node.left:
-                q.append((node.left,ans+str(node.val)+'->'))
             if node.right:
                 q.append((node.right,ans+str(node.val)+'->'))
+             
+            if node.left:
+                q.append((node.left,ans+str(node.val)+'->'))
         return res
