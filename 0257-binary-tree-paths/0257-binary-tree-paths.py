825011361d9bebb7a33a35b7f78fa10fc6ceb911
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-19 13:00:39 +0530

    Time: 70 ms (19.27%), Space: 13.9 MB (30.87%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,15 +6,16 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        res=[]
-        q=[(root,'')]
-        while q:
-            node,ans=q.pop()
+        if not root:
+            return []
+        res, queue = [], collections.deque([(root, "")])
+        while queue:
+            node, ls = queue.popleft()
             if not node.left and not node.right:
-                res.append(ans+str(node.val))
-            if node.right:
-                q.append((node.right,ans+str(node.val)+'->'))
-             
+                res.append(ls+str(node.val))
             if node.left:
-                q.append((node.left,ans+str(node.val)+'->'))
+                queue.append((node.left, ls+str(node.val)+"->"))
+            if node.right:
+                queue.append((node.right, ls+str(node.val)+"->"))
         return res
+        
