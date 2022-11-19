16986cc3ba22841e8ef53845e47090b140f7bb21
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-19 13:07:36 +0530

    Time: 67 ms (29.02%), Space: 14 MB (30.87%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -15,7 +15,7 @@ class Solution:
     def dfs(self, root, ls, res):
         if not root.left and not root.right:
             res.append(ls+str(root.val))
+        if root.right:
+            self.dfs(root.right, ls+str(root.val)+"->", res)            
         if root.left:
             self.dfs(root.left, ls+str(root.val)+"->", res)
-        if root.right:
-            self.dfs(root.right, ls+str(root.val)+"->", res)
