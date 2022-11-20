75d4e29ea6007923144fed4a92b9b4b2b43f6fc0
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-20 13:11:53 +0530

    Time: 39 ms (98.54%), Space: 15.2 MB (15.99%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -1,14 +1,14 @@
 class Solution:
     def hasPathSum(self, root, sum):
-        if not root:
-            return False
-        queue = [(root, sum-root.val)]
-        while queue:
-            curr, val = queue.pop(0)
-            if not curr.left and not curr.right and val == 0:
-                return True
-            if curr.left:
-                queue.append((curr.left, val-curr.left.val))
-            if curr.right:
-                queue.append((curr.right, val-curr.right.val))
-        return False
+        res = []
+        self.dfs(root, sum, res)
+        return any(res)
+
+    def dfs(self, root, target, res):
+        if root:
+            if not root.left and not root.right and root.val == target:
+                res.append(True)
+            if root.left:
+                self.dfs(root.left, target-root.val, res)
+            if root.right:
+                self.dfs(root.right, target-root.val, res)
