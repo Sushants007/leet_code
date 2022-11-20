0e57914be21004d3126f3fd7bef732b244560bfa
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-20 16:44:03 +0530

    Time: 597 ms (24.43%), Space: 17.9 MB (30.63%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -1,24 +1,13 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
-        maxlevel = 0
-        tsum = 0
-
-        def dfs(node=root, level=0):
-            nonlocal maxlevel, tsum
-            if not node: return
-            dfs(node.left, level+1)
-            dfs(node.right, level+1)
-            if maxlevel < level:
-                maxlevel = level
-                tsum = 0
-            if maxlevel == level:
-                tsum += node.val
-
-        dfs()
-        return tsum
+        queue = [root]
+        while queue:
+            curr = 0
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                curr = curr + node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return curr
