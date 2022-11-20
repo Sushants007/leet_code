b5eb93032796e07b854daeae8bda1dd0e2e3160c
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-20 13:52:03 +0530

    Time: 330 ms (19.36%), Space: 14.9 MB (87.07%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -1,36 +1,12 @@
-
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        stack = [root]
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not s: 
+            return False
+        if self.isSameTree(s, t): 
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
 
-        while stack:
-            node = stack.pop()
-
-            if node.val==subRoot.val:
-                if self.check(node, subRoot):
-                    return True
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-        return False
-
-    def check(self, r:TreeNode, s:TreeNode) -> bool:
-        stack = [(r, s)]
-
-        while stack:
-            node1, node2 = stack.pop()
-            #print(node1, node2)
-            if node1.val!=node2.val:
-                return False
-            if node1.left or node2.left:
-                if (node1.left and not node2.left) or (not node1.left and node2.left):
-                    return False    
-                else:
-                    stack.append((node1.left, node2.left))
-            if node1.right or node2.right:
-                if (node1.right and not node2.right) or (not node1.right and node2.right):
-                    return False
-                else:
-                    stack.append((node1.right, node2.right))
-        return True
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
