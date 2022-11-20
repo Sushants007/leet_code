82ad68f352f6968baaab8e5dee6176606b87606b
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-20 12:42:39 +0530

    Time: 61 ms (88.99%), Space: 18.5 MB (40.52%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -1,28 +1,20 @@
 class Solution:
-    def findMode(self, root: TreeNode) -> List[int]:
-        
-        lst = []                            
-        dic = {}                            
-        maxi = 0                           
-        
-        def find(root):
-            
-            if not root:                    
-                return
-            
-            if root.val in dic:            
-                dic[root.val] += 1
-            else:
-                dic[root.val] = 1          
-            find(root.left)
-            find(root.right)
-            
-            return 
-     
-        find(root)
-        dicval_list = list(dic.values())    
-        maxi = max(dicval_list)             
-        for key, value in dic.items():
-            if value == maxi:               
-                lst.append(key)
-        return lst
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        q = collections.deque()
+        q.append(root)
+        maxi = 0
+        arr = []
+        d = collections.defaultdict(lambda: 0)
+        while len(q) > 0:
+            front = q.popleft()
+            d[front.val] += 1
+            if d[front.val] > maxi:
+                maxi = d[front.val]
+                arr = [front.val]
+            elif d[front.val] == maxi:
+                arr.append(front.val)
+            if front.left: 
+                q.append(front.left)
+            if front.right:
+                q.append(front.right)
+        return arr
