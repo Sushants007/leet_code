5ddc135cdf5be6568b8fa04a32f521eef16308d0
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-20 17:46:10 +0530

    Time: 235 ms (48.79%), Space: 17.7 MB (92.34%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -1,28 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def sumEvenGrandparent(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        q = [root]
-        s = 0
-        while q:
-            curr = q.pop()
-            if curr:
-                q+=[curr.right, curr.left]
-                if curr.val%2==0:
-                    if curr.left:
-                        if curr.left.left:
-                            s+=curr.left.left.val
-                        if curr.left.right:
-                            s+=curr.left.right.val
-                    if curr.right:
-                        if curr.right.left:
-                            s+=curr.right.left.val
-                        if curr.right.right:
-                            s+=curr.right.right.val
-        return s
+    def sumEvenGrandparent(self, node: TreeNode) -> int:
+        self.total = 0
+        
+        def dfs(root):
+            if root.val % 2 == 0:
+                if root.left:
+                    if root.left.left:
+                        self.total += root.left.left.val 
+                    if root.left.right:
+                        self.total += root.left.right.val
+                if root.right:
+                    if root.right.left:
+                        self.total += root.right.left.val
+                    if root.right.right:
+                        self.total += root.right.right.val
+            if root.left:
+                dfs(root.left)
+            if root.right:
+                dfs(root.right)
+            return
+        
+        dfs(node)
+        return self.total
