f5c6bfc87875ca3346c07f95ac1a55366d2aba68
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-21 12:01:56 +0530

    Time: 554 ms (73.02%), Space: 17.9 MB (94.20%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -1,17 +1,21 @@
-
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        arr=[]
-        def dfs(node):
-            nonlocal arr
-            if not node:
-                return 0
-            arr.append(node.val)
-
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root1)
-        dfs(root2)
-        print(arr)
-        return sorted(arr)
-            
+        st1, st2 = [], []
+        res = []
+        
+        while root1 != None or root2 != None or len(st1) > 0 or len(st2) > 0:
+            while root1 != None:
+                st1.append(root1)
+                root1 = root1.left
+            while root2 != None:
+                st2.append(root2)
+                root2 = root2.left
+            if len(st2) == 0 or (len(st1) > 0 and st1[-1].val <= st2[-1].val):
+                root1 = st1.pop()
+                res.append(root1.val)
+                root1 = root1.right
+            else:
+                root2 = st2.pop()
+                res.append(root2.val)
+                root2 = root2.right
+        return res
