6a92c02d677dbe143bbe01cf4ddc2f37ff6da9e6
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-21 11:35:29 +0530

    Time: 1065 ms (43.31%), Space: 21.7 MB (22.64%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -1,20 +1,20 @@
 class Solution:
 	def balanceBST(self, root: TreeNode) -> TreeNode:
-		v = []
+		arr = []
 		def dfs(node):
 			if node:
 				dfs(node.left)
-				v.append(node.val)
+				arr.append(node.val)
 				dfs(node.right)
 		dfs(root)
 
-		def bst(v):
-			if not v:
+		def bst(nums):
+			if not nums:
 				return None
-			mid = len(v) // 2
-			root = TreeNode(v[mid])
-			root.left = bst(v[:mid])
-			root.right = bst(v[mid + 1:])
+			mid = len(nums) // 2
+			root = TreeNode(nums[mid])
+			root.left = bst(nums[:mid])
+			root.right = bst(nums[mid + 1:])
 			return root
 
-		return bst(v)
+		return bst(arr)
