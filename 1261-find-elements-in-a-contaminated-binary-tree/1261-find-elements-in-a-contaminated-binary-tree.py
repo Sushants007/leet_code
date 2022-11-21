d7f3bef61f9714bddba7031be9c7be86204a61d6
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-21 12:23:17 +0530

    Time: 232 ms (30.06%), Space: 18.6 MB (8.04%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
@@ -1,14 +1,18 @@
-class FindElements(object):
+class FindElements:
     def __init__(self, root):
         self.A = A = set()
-        #
-        def recover(n,x):
-            if n:
-                A.add(x)
-                recover(n.left , 2*x + 1)
-                recover(n.right, 2*x + 2)
-        #
-        recover(root,0)
-    #
+
+        if not root:
+            return
+        
+        queue = collections.deque([[root,0]])
+        while queue:
+            n,x = queue.popleft()
+            A.add(x)
+            if n.left:
+                queue.append( [n.left  , 2*x+1] )
+            if n.right:
+                queue.append( [n.right , 2*x+2] )
+    
     def find(self, target):
         return target in self.A
