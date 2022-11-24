508ca6c19ec3cb79964a26951c527a5167c8785b
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-24 16:20:37 +0530

    Time: 683 ms (61.23%), Space: 16.7 MB (42.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,23 @@
-from collections import deque
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0   
         count = 0
-        check = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] =='1' and check[i][j]== False:
-                    count += 1
-                    self.bfs(grid,check,i,j)
-        return count       
-    def bfs(self,grid,check,i,j):
-        qu = deque([(i,j)])
-        while qu:
-            i, j = qu.popleft()
-            if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j]=='1' and check[i][j]==False:
-                check[i][j] = True
-                qu.extend([(i-1,j),(i+1,j),(i,j-1),(i,j+1)])
+                if grid[i][j] == '1':
+                    print(i,j)
+                    self.dfs(grid,i,j)
+                    count  += 1
+        #print(grid)
+        return count
+    # use a helper function to flip connected '1's to 0
+    def dfs(self,grid,i,j):
+        grid[i][j] = 0
+        for dr,dc in (1,0), (-1,0), (0,-1), (0,1):
+            r = i + dr
+            c = j + dc
+            if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c]=='1':
+                self.dfs(grid,r,c)
+
                 
     # def bfs(self,grid,check,i,j):
     #     q=deque((i,j))
