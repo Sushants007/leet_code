ae1e5605fb244ef315eacce4c02677a5eca05c0f
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-25 12:32:55 +0530

    Time: 97 ms (64.93%), Space: 14 MB (11.92%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,42 +1,21 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows=len(grid)
-        
-        if rows ==0:
-            return -1
-        
-        cols=len(grid[0])
-        fresh=0
-        rotten=deque()
-        
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]==2:
-                    rotten.append((i,j))
-                elif grid[i][j]==1:
-                    fresh+=1
-                    
-        min=0
-        
-        while rotten and fresh>0:
-            
-            
-            for _ in range(len(rotten)):
-                 
-                x,y = rotten.popleft()
-                for dx,dy in [(0,1),(1,0),(-1,0),(0,-1)]:
-                    xx=x+dx
-                    yy=y+dy
-                    if xx<0 or yy<0 or xx>=rows or yy>=cols: 
-                        continue
-                    if grid[xx][yy]==0 or grid[xx][yy]==2: 
-                        continue
-                       
-                    fresh-=1
-                    grid[xx][yy]=2
-                    rotten.append((xx,yy))
-            min+=1                    
-        if fresh==0:
-            return min        
-        else:
-            return -1        
+        row, col = len(grid), len(grid[0])
+        dirs = [(-1,0),(0,1),(1,0),(0,-1)]
+        fresh_set=set()
+        rotten = collections.deque()
+        step = 0
+        for x in range(row):
+            for y in range(col):
+                if grid[x][y]==1:
+                    fresh_set.add((x,y))
+                elif grid[x][y]==2:
+                    rotten.append([x,y,step])
+        while rotten:
+            x,y,step = rotten.popleft()
+            for dx, dy in dirs:
+                if 0<=x+dx<row and 0<=y+dy<col and grid[x+dx][y+dy] == 1:
+                    grid[x+dx][y+dy]=2
+                    fresh_set.remove((x+dx,y+dy))
+                    rotten.append([x+dx,y+dy,step+1])
+        return step if not fresh_set else -1
