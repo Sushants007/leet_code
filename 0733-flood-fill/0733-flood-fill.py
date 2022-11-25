8d09062e7f883137046063f613d208237ef966c4
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-25 12:02:12 +0530

    Time: 181 ms (31.88%), Space: 13.9 MB (90.33%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,18 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m=len(image)
+        n=len(image[0])
+        source=image[sr][sc]
+        q=deque([(sr,sc)])
+        while q:
+            i,j=q.popleft()
+            if i<0 or j<0 or i>=m or j>=n or image[i][j]==color or image[i][j]!=source:
+                continue
+            image[i][j]=color
+            q.append((i+1,j))
+            q.append((i-1,j))
+            q.append((i,j+1))
+            q.append((i,j-1))
+            
+        return image
+        return imagecopy
