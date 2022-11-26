60223d6f9a52b3ad5b27961f11a04ece067a6735
Sushants007 <102995828+Sushants007@users.noreply.github.com>
2022-11-26 13:40:37 +0530

    Time: 607 ms (99.72%), Space: 75.7 MB (61.19%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,44 +1,32 @@
 class Solution:
-    def numEnclaves(self, grid: List[List[int]]) -> int:
-        d=[[-1,0],[0,1],[1,0],[0,-1]]
-        q=[]
-        count=0
-        r=len(grid)
-        c=len(grid[0])
-        # for i in range(0,r):
-        #     for j in range(0,c):
-        #         if i==0 or j==0 or i==r-1 or j==c-1:
-        #             if grid[i][j]==1:
-        #                 grid[i][j]=-1
-        #                 q.append((i,j))
-        for i in range(0,r):         
-            if grid[i][0]==1:
-                grid[i][0]=-1
-                q.append((i,0))
-            if grid[i][c-1]==1:
-                grid[i][c-1]=-1
-                q.append((i,c-1))
-        for j in range(0,c):                    
-            if grid[0][j]==1:
-                grid[0][j]=-1
-                q.append((0,j))
-            if grid[r-1][j]==1:
-                grid[r-1][j]=-1
-                q.append((r-1,j))
+    def numEnclaves(self, A: List[List[int]]) -> int:
 
-        while q:
-            curr=q[0]
-            q.remove(q[0])
-            row=curr[0]
-            col=curr[1]
-            for di,dj in d:
-                curRow=row+di
-                curCol=col+dj
-                if curRow>=1 and curCol>=1 and curRow<r-1 and curCol<c-1 and grid[curRow][curCol]==1:
-                    grid[curRow][curCol]=-1
-                    q.append((curRow,curCol))
-        for i in range(r):
-            for j in range(c):  
-                    if(grid[i][j]==1):
-                        count+=1
-        return count                
+        row, col = len(A), len(A[0])
+
+        if not A or not A[0]:
+            return 0
+
+        def dfs(x,y,A):
+            if 0<=x<row and 0<=y<col and A[x][y] ==1:
+                A[x][y] = "T"
+                dfs(x+1,y,A)
+                dfs(x-1,y,A)
+                dfs(x,y+1,A)
+                dfs(x,y-1,A)
+
+
+        for x in range(row):
+            dfs(x,0,A)
+            dfs(x,col-1,A)
+
+        for y in range(1,col-1):
+            dfs(0,y,A)
+            dfs(row-1,y,A)
+
+        count=0
+        for x in range(row):
+            for y in range(col):
+                if A[x][y]==1:
+                    count+=1
+
+        return count               
